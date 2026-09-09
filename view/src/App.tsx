e1e7738c9ceb7fe227ba788acc2831876bba41a6
nasraabdirahman <nasrafaisal72@gmail.com>
2026-09-09 17:00:03 +0200

    changed folder name

---
+++ view/src/App.tsx
@@ -0,0 +1,12 @@
+
+import { Typography } from "@mui/material"
+function App() {
+    return (
+    <Typography variant="h1">
+        Hello world
+    </Typography>
+    )
+
+}
+
+export default App
