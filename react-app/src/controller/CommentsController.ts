30a21d4d99971f22cbaac765e9095b0999118e8e
issaabellaa <isaabella.lazar@hotmail.com>
2026-09-15 22:40:43 +0200

    Update CommentsController and add unit tests

--- react-app/src/controller/CommentsController.ts
+++ react-app/src/controller/CommentsController.ts
@@ -1,12 +1,14 @@
 import type { Comment } from "../model/Comments";
 import { getComments } from "../DB/commentdata";
 
-export function PostComment(comment: Comment): void {
+export class CommentsController {
+
+    PostComment(comment: Comment): void {
     const comments = getComments();
     comments.push(comment);
 }
 
-export function DeleteComment(commentid: number): void {
+DeleteComment(commentid: number): void {
     const comments = getComments();
     const index = comments.findIndex(comment => comment.commentid === commentid);
 
@@ -15,12 +17,13 @@ export function DeleteComment(commentid: number): void {
     }
 }
 
-export function editComment(commentid: number, newCommentData: string): void {
+editComment(commentid: number, newCommentData: string): void {
 
     const comments = getComments();
     const comment = comments.find(comment => comment.commentid === commentid);
     if (comment) {
         comment.commentData = newCommentData;
 
+}
 }
 }
