d53b3a76e930e5034c04a419dbf62ca5f8284211
nasraabdirahman <nasrafaisal72@gmail.com>
2026-09-16 09:54:55 +0200

    fixed ci issue

--- react-app/src/unit_test/getdata.test.ts
+++ react-app/src/unit_test/getdata.test.ts
@@ -1,12 +1,12 @@
 import { getAllComments } from "../model/service/getComments";
 import { getAllUsers } from "../model/service/getUser";
-import { getAllEvents } from "../model/service/getEvents";
+import { ModelServicesEvent } from "../model/service/getEvents";
 import { test } from 'vitest'
 
 test("get data from DB map", () =>{
     const comments = getAllComments();
     const users = getAllUsers();
-    const events = getAllEvents();
+    const events = new ModelServicesEvent();
 
-    console.log(comments, users, events);
+    console.log(comments, users, events.getAllEvents());
 });
