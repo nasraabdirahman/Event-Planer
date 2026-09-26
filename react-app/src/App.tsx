1248fa2662fccdc03a014c0d8ac2730ed03967d6
nasraabdirahman <nasrafaisal72@gmail.com>
2026-09-26 12:50:39 +0200

    add react router for sign in and calender

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,24 +4,28 @@ import {useState} from 'react'
 import ThemeButton from './view/theme/themeButton'
 import { ThemeContext } from './view/theme/colourTheme';
 import Calendar from './view/components/calendar/calendar.tsx'
-import Footer from './footer.tsx'
+import Footer from '../src/view/components/footer.tsx'
 import SignIn from './sign-in/SignIn.tsx'
+import {BrowserRouter, Routes, Route} from "react-router";
 
 function App() {
   const [theme, setTheme] = useState('Classic');
 
   return (
   <>
-    <SearchBar />
-
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className={`theme-${theme}`}>
-        <SignIn />
-        <Calendar userId={1} />
-        <ThemeButton />
-        <Footer />
-      </div>
-    </ThemeContext.Provider>
+  <BrowserRouter>
+      <SearchBar />
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <div className={`theme-${theme}`}>
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/calendar" element={<Calendar userId={1}/>}/>
+          </Routes>
+          <ThemeButton />
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
+    </BrowserRouter>
   </>
 )
 }
